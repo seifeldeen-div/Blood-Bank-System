2b7ac5fd9b9785ac1ea77f3486a0cae5de0a9c55
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-03-09 18:00:20 +0200

    about contact page done

--- assets/js/load.js
+++ assets/js/load.js
@@ -15,6 +15,15 @@ fetch(`${basePath}/components/navbar.html`)
         // let links = document.querySelector("#links");
         let lis = document.querySelectorAll("#links ul li")
         let container = document.querySelector(".container")
+        let links = document.querySelectorAll(".link")
+
+        links.forEach((link) => {
+            link.addEventListener("click", (e) => {
+                // e.preventDefault()
+                links.forEach(l => l.classList.remove("activeLink"))
+                link.classList.add("activeLink")
+            })
+        })
 
         burgerSign.addEventListener("click", () => {
             // let currentDisplay = window.getComputedStyle(links).display;
---
+++ assets/js/pages/aboutContact.js
@@ -0,0 +1,77 @@
+let magicContactbtn = document.querySelector(".magicContactbtn")
+let contactSec = document.querySelector(".contact")
+let counter = document.querySelector("#counter")
+let textMessage = document.querySelector("#textMessage")
+let countLimit = 10
+let warningMessaage = document.querySelector("#warningMessaage")
+let form = document.querySelector(".contact form")
+let btnSubmitMessage = document.querySelector(".btnSubmitMessage input[type='submit']")
+let SubmitMessage = document.querySelector('.SubmitMessage')
+
+WordLength()
+
+form.addEventListener("submit", (e) => {
+    if (!CheckWordLength())
+        e.preventDefault()
+    else {
+        e.preventDefault()
+        SubmitMessage.classList.add("done")
+        setTimeout(()=>{
+            SubmitMessage.classList.remove("done")
+        },1000)
+        form.reset()
+        counter.textContent = ""
+    }
+})
+
+magicContactbtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    contactSec.classList.toggle("contactActive")
+})
+
+function WordLength() {
+    textMessage.addEventListener("input", (e) => {
+        e.preventDefault()
+        counter.innerHTML = "count: " + textMessage.value.length
+        if (textMessage.value.length > countLimit) {
+            textMessage.classList.add("maxCount")
+            warningMessaage.textContent = `Max length = ${countLimit} letter`
+            return
+        }
+        else {
+            textMessage.classList.remove("maxCount")
+            warningMessaage.textContent = ""
+        }
+    })
+}
+function WordLength() {
+    let check = true;
+    textMessage.addEventListener("input", () => {
+        counter.innerHTML = "count: " + textMessage.value.length
+        if (textMessage.value.length > countLimit) {
+            textMessage.classList.add("maxCount")
+            warningMessaage.textContent = `Max length = ${countLimit} letter`
+            check = false;
+        }
+        else {
+            textMessage.classList.remove("maxCount")
+            warningMessaage.textContent = ""
+        }
+    })
+    if (check)
+        return true
+    else
+        return false
+}
+function CheckWordLength() {
+    if (textMessage.value.length > countLimit) {
+        textMessage.classList.add("maxCount")
+        warningMessaage.textContent = `Max length = ${countLimit} letter`
+        return false
+    }
+    else {
+        textMessage.classList.remove("maxCount")
+        warningMessaage.textContent = ""
+        return true
+    }
+}
